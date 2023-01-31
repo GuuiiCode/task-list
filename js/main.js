c2049caf60368c13a3fe5fd178ab095d8c3009b1
GuuiiCode <guilherme.job07@gmail.com>
2023-01-31 19:04:31 -0300

    salvando no localStorage

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ const inputTask = document.querySelector('.input-task');
 
 const btnAddTask = document.querySelector('.btn-add-task');
 
-const ul = document.querySelector('.task');
+const ul = document.querySelector('.task-list');
+
+let task_list = [];
 
 btnAddTask.addEventListener('click', () => {
-
     if (inputTask.value) {
         let li = document.createElement('li');
         li.classList.add('content');
@@ -26,7 +27,6 @@ btnAddTask.addEventListener('click', () => {
         i.classList.add('fa');
         i.classList.add('fa-trash');
 
-
         ul.appendChild(li);
 
         li.appendChild(div);
@@ -41,7 +41,9 @@ btnAddTask.addEventListener('click', () => {
 
         span.innerHTML = inputTask.value;
 
-        inputTask.value = "";
+        inputTask.value = '';
+
+        createStorage(span.innerHTML);
 
         button.addEventListener('click', () => {
             ul.removeChild(li);
@@ -50,5 +52,58 @@ btnAddTask.addEventListener('click', () => {
 
     inputTask.focus();
 });
+
+function createStorage(content) {
+    task_list.push(content);
+    localStorage.setItem('tarefas', JSON.stringify(task_list));
+}
+
+const getStorage = () => {
+    const tasks = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (tasks) {
+        for (let task of tasks) {
+            let li = document.createElement('li');
+            li.classList.add('content');
+
+            let div = document.createElement('div');
+            div.classList.add('content-left');
+
+            let input = document.createElement("input");
+            input.setAttribute('type', 'checkbox');
+            input.classList.add('checkbox-task');
+
+            let span = document.createElement('span');
+
+            let button = document.createElement('button');
+            button.classList.add('btn-delete-task');
+
+            let i = document.createElement('i');
+            i.classList.add('fa');
+            i.classList.add('fa-trash');
+
+            ul.appendChild(li);
+
+            li.appendChild(div);
+
+            div.appendChild(input);
+
+            div.appendChild(span);
+
+            li.appendChild(button);
+
+            button.appendChild(i);
+
+            span.innerHTML = task;
+
+            task_list.push(span.innerHTML);
+        }
+    }
+}
+
+getStorage();
+
+
+
 
 
