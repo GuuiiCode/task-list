324bea71211c159252e6553cdc6e4159738cad70
GuuiiCode <guilherme.job07@gmail.com>
2023-02-01 00:39:58 -0300

    refatorações e ação de deletar do localStore

--- js/main.js
+++ js/main.js
@@ -4,106 +4,107 @@ const btnAddTask = document.querySelector('.btn-add-task');
 
 const ul = document.querySelector('.task-list');
 
-let task_list = [];
+const KEY_LOCAL_STORAGE = 'taskList';
+
+let db_tasks = [];
+
 
 btnAddTask.addEventListener('click', () => {
     if (inputTask.value) {
-        let li = document.createElement('li');
-        li.classList.add('content');
+        let tasks = {
+            id: generateId(),
+            name: inputTask.value
+        }
 
-        let div = document.createElement('div');
-        div.classList.add('content-left');
-
-        let input = document.createElement("input");
-        input.setAttribute('type', 'checkbox');
-        input.classList.add('checkbox-task');
-
-        let span = document.createElement('span');
-
-        let button = document.createElement('button');
-        button.classList.add('btn-delete-task');
-
-        let i = document.createElement('i');
-        i.classList.add('fa');
-        i.classList.add('fa-trash');
-
-        ul.appendChild(li);
-
-        li.appendChild(div);
-
-        div.appendChild(input);
-
-        div.appendChild(span);
-
-        li.appendChild(button);
-
-        button.appendChild(i);
-
-        span.innerHTML = inputTask.value;
-
+        createElement(tasks);
+        addTasks(tasks);
         inputTask.value = '';
-
-        createStorage(span.innerHTML);
-
-        button.addEventListener('click', () => {
-            ul.removeChild(li);
-        });
     }
 
     inputTask.focus();
 });
 
-function createStorage(content) {
-    task_list.push(content);
-    localStorage.setItem('tarefas', JSON.stringify(task_list));
+/**Add tasks**/
+function addTasks(task) {
+    db_tasks.push(task);
+    saveStorage();
 }
 
-const getStorage = () => {
-    const tasks = JSON.parse(localStorage.getItem('tarefas'));
+/**Save local storage**/
+function saveStorage() {
+    localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(db_tasks));
+}
 
-    if (tasks) {
-        for (let task of tasks) {
-            let li = document.createElement('li');
-            li.classList.add('content');
+/**Remove local storage**/
+function deleteStorage(id) {
+    let confirm = window.confirm('Are you sure you want to delete?')
+    if (confirm) {
+        const taskIndex = db_tasks.findIndex(f => f.id == id);
 
-            let div = document.createElement('div');
-            div.classList.add('content-left');
-
-            let input = document.createElement("input");
-            input.setAttribute('type', 'checkbox');
-            input.classList.add('checkbox-task');
-
-            let span = document.createElement('span');
-
-            let button = document.createElement('button');
-            button.classList.add('btn-delete-task');
-
-            let i = document.createElement('i');
-            i.classList.add('fa');
-            i.classList.add('fa-trash');
-
-            ul.appendChild(li);
-
-            li.appendChild(div);
-
-            div.appendChild(input);
-
-            div.appendChild(span);
-
-            li.appendChild(button);
-
-            button.appendChild(i);
-
-            span.innerHTML = task;
-
-            task_list.push(span.innerHTML);
+        if (taskIndex < 0) {
+            throw new Error("Task id not found.")
         }
+
+        db_tasks.splice(taskIndex, 1);
+        saveStorage();
+
+        location.reload();
     }
 }
 
+/**Recovery local storage**/
+const getStorage = () => {
+    const tasks = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE));
+
+    if (tasks) {
+        db_tasks = tasks;
+        db_tasks.forEach(task => {
+            createElement(task);
+        });
+    }
+}
+
+/**Render elements**/
 getStorage();
 
+/**Create id**/
+function generateId() {
+    return Math.floor(Date.now() * Math.random()).toString(36)
+}
 
+/**Build initialization variables**/
+function createElement(task) {
+    let li = document.createElement('li');
+    li.classList.add('content');
 
+    let div = document.createElement('div');
+    div.classList.add('content-left');
 
+    let input = document.createElement("input");
+    input.setAttribute('type', 'checkbox');
+    input.classList.add('checkbox-task');
 
+    let span = document.createElement('span');
+
+    let button = document.createElement('button');
+    button.setAttribute('onclick', `deleteStorage('${task.id}')`);
+    button.classList.add('btn-delete-task');
+
+    let i = document.createElement('i');
+    i.classList.add('fa');
+    i.classList.add('fa-trash');
+
+    ul.appendChild(li);
+
+    li.appendChild(div);
+
+    div.appendChild(input);
+
+    div.appendChild(span);
+
+    li.appendChild(button);
+
+    button.appendChild(i);
+
+    span.innerHTML = task.name;
+}
